a29ba36d1a763cd4a62fdbbcd38b4e366d481d66
irvanpreet <irvan.dhillon.10@gmail.com>
2016-08-23 19:53:42 +1000

    Update IStudentUnitRecord.java

--- ITC515_assignment2/src/datamanagement/IStudentUnitRecord.java
+++ ITC515_assignment2/src/datamanagement/IStudentUnitRecord.java
@@ -1,18 +1,16 @@
 package datamanagement;
+import java.util.*;
+import java.io.*;
 
-public interface IStudentUnitRecord {
-
-    public Integer getStudentID();
-    public String getUnitCode();
-
-    public void setAsg1(float mark);
-    public float getAsg1();
-
-    public void setAsg2(float mark);
-    public float getAsg2();
-
-    public void setExam(float mark);
-    public float getExam();
-
-    public float getTotal();
+public interface IStudentUnitRecord
+{
+  public Integer getStudentID();
+  public String getUnitCode();
+  public void setAsg1(float mark);
+  public float getAsg1();
+  public void setAsg2(float mark);
+  public float getAsg2();
+  public void setExam(float mark);
+  public float getExam();
+  public float getTotal();
 }
