cffe92841059fee0ed025ab7a0e62762c9009faf
irvanpreet <irvan.dhillon.10@gmail.com>
2016-08-25 21:09:20 +1000

    Update ListStudentsCTL.java

--- ITC515_assignment2/src/datamanagement/ListStudentsCTL.java
+++ ITC515_assignment2/src/datamanagement/ListStudentsCTL.java
@@ -10,10 +10,10 @@ public class ListStudentsCTL
     sm = StudentManager.get();
          
  }
-    public void listStudents( IStudentLister lister, String unitCode )
+    public void listStudents(IStudentLister lister, String unitCode)
  {
     lister.clearStudents();
-    StudentMap students = sm.getStudentsByUnit( unitCode );
+    StudentMap students = sm.getStudentsByUnit(unitCode);
     for (Integer id : students.keySet()) lister.addStudent(students.get(id));
  }
 }
