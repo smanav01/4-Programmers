571ebb48ad112613e30198d2163bbd315b233d82
Shikha Manav <shik.mnv02@gmail.com>
2016-10-09 01:14:25 +1100

    bug 1-balance correction
    corrected balance calculation

--- ITC515_assignment4/CrownAndAnchorGame/src/src/Game.java
+++ ITC515_assignment4/CrownAndAnchorGame/src/src/Game.java
@@ -29,7 +29,7 @@ public class Game {
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
 		
-		player.takeBet(bet);
+		
 		    
 		int matches = 0;
 		for ( Dice d : dice) {
@@ -43,6 +43,9 @@ public class Game {
 
 		if (matches > 0) {			
 			player.receiveWinnings(winnings);
+		}
+		else {
+			player.takeBet(bet); // corrected balance calculation
 		}
         return winnings;		
 	}
