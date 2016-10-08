b9bef993a8e4144e31b245dec83467b6c6a5a04e
Shikha Manav <shik.mnv02@gmail.com>
2016-10-09 01:17:20 +1100

    bug 3- dice rolling
    corrected dice rolling

--- ITC515_assignment4/CrownAndAnchorGame/src/src/Main.java
+++ ITC515_assignment4/CrownAndAnchorGame/src/src/Main.java
@@ -7,13 +7,8 @@ public class Main {
 		
 	   BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-        Dice d1 = new Dice();
-        Dice d2 = new Dice();
-        Dice d3 = new Dice();
 
         Player player = new Player("Fred", 100);
-        Game game = new Game(d1, d2, d3);
-        List<DiceValue> cdv = game.getDiceValues();
 
         int totalWins = 0;
         int totalLosses = 0;
@@ -39,7 +34,12 @@ public class Main {
                 int turn = 0;
                 while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
                 {
-                    turn++;                    
+                    turn++;             
+		    Dice d1 = new Dice(); //corrected the values of dice that comes randomly
+                    Dice d2 = new Dice();
+                    Dice d3 = new Dice();
+                    Game game = new Game(d1, d2, d3);
+                    List<DiceValue> cdv = game.getDiceValues();       
                 	DiceValue pick = DiceValue.getRandom();
                    
                 	System.out.printf("Turn %d: %s bet %d on %s\n",
